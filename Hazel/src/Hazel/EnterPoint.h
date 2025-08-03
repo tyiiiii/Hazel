e6cd898bb491e582e9aab33807e67a51d3d00b0b
tyi <3554762558@qq.com>
2025-08-04 00:44:34 +0800

    新增日志库

--- Hazel/src/Hazel/EnterPoint.h
+++ Hazel/src/Hazel/EnterPoint.h
@@ -4,8 +4,11 @@
 extern Hazel::Application* Hazel::CreateApplication();
  
 int main(int argc, char **argv) {
+	Hazel::Log::Init();
+	HZ_CORE_ERROR("test");
+	HZ_ERROR("test2");
+
 	auto app = Hazel::CreateApplication();
-	//cout << "hello world";
 	app->run();
 	delete app;
 	return 0;
